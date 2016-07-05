797d1e3210a11ee61beb6e9acea5b2175e4ff8a3
Toshiyuki Masui <masui@pitecan.com>
2016-07-05 22:29:33 +0100

    jquery追加

--- episopass.js
+++ episopass.js
@@ -1,13 +1,8 @@
 //
-// ChromeでもFirefoxでも使える拡張機能
+// Facebookなどのログイン画面でEpisoPassを呼び出すブラウザ拡張機能
+// ChromeでもFirefoxでも使えるはず
 //
 $(function(){
-    //
-    // <input type="password" class="inputtext" name="pass" id="pass" tabindex="2" />
-    //
-
-    // $('body').css('border',"30px solid yellow");
-
     var passinput = [];
     var episourl = null;
     if(location.href.match(/facebook.com/)){
@@ -22,18 +17,4 @@ $(function(){
 	    window.clicked = true;
 	});
     }
-
-    //        //window.showModalDialog("http://episopass.com/masui2015/Facebook123456","xxx","width=800,height=600,menubar=no,toolbar=no,location=no,scrollbars=yes");
-    //        //var xxx = window.showModalDialog("http://episopass.com/masui2015/Facebook123456",window,"dialogWidth=600;dialogHeight=500;");
-    //        // alert(xxx);
-    //        //alert(window.returnValue);
-    //
-    //        //spawn(function*() {
-    //        //  var ret = yield window.showModalDialog("http://episopass.com/masui2015/Facebook123456.html", "some argument", "dialogWidth:600px;dialogHeight:500px");
-    //        //  alert("Returned from modal: " + ret);
-    //        //});
-    //        
-    //        window.open("http://episopass.com/masui2015/Facebook123456");
-    //    }
-    // }
 });
